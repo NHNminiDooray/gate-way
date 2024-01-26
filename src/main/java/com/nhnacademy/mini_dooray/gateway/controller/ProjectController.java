f948e90446e471a9b2c8632d2b26714ade6750ba
rkdrjsrks <rkdrjs71@naver.com>
2024-01-26 19:51:28 +0900

    feat : add ProjectController

---
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/controller/ProjectController.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.mini_dooray.gateway.controller;
+
+import com.nhnacademy.mini_dooray.gateway.domain.ProjectDto;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class ProjectController {
+//    private ProjectService projectService;
+
+    @GetMapping
+    public String showProjects(Model model) {
+//        List<ProjectDto> projectList = projectService.getAllProjects();
+//        model.addAttribute("projectList", projectList);
+        return "projectList";
+    }
+    @GetMapping("/create")
+    public String showProjectCreationForm() {
+        return "newproject";
+    }
+}
