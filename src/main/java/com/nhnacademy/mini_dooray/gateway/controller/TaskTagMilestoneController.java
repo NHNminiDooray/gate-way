595688b847e5b2a096a04372e5d425a5212ec864
rkdrjsrks <rkdrjs71@naver.com>
2024-01-28 00:23:35 +0900

    feat : 업무 안 태그 , 마일스톤 등록,삭제 기능 추가

--- src/main/java/com/nhnacademy/mini_dooray/gateway/controller/TaskTagMilestoneController.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/controller/TaskTagMilestoneController.java
@@ -2,10 +2,7 @@ package com.nhnacademy.mini_dooray.gateway.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/projects/{projectId}/tasks/{taskId}/manage")
@@ -14,24 +11,44 @@ public class TaskTagMilestoneController {
     public String showTaskTags(Model model, @PathVariable Long projectId, @PathVariable Long taskId) {
         model.addAttribute("projectId",projectId);
         model.addAttribute("taskId",taskId);
-        return  "";
+        //TODO
+        //테스크 안의 태그 리스트(삭제 하기 위함) 및 모든 태그 리스트 (추가 하기 위함)
+//        model.addAttribute("taskTagList",taskTagList);
+//        model.addAttribute("tagList",tagList);
+        return  "taskTagManagement";
     }
-    @PostMapping("/milestone/delete")
-    public String deleteTag(@PathVariable Long projectId, @PathVariable Long taskId) {
+    @PostMapping("/tag/add")
+    public String addTag(@PathVariable Long projectId, @PathVariable Long taskId, @RequestParam Long tagId) {
+        //TODO
+        // 테스크 안의 태그 추가 로직
+
+        return "redirect:/projects/" + projectId + "/tasks/" + taskId;
+    }
+    @DeleteMapping("/tag/delete/{tagId}")
+    public String deleteTag(@PathVariable Long projectId, @PathVariable Long taskId, @PathVariable Long tagId) {
         //TODO
         // 테스크 안의 태그 삭제 로직
 
         return "redirect:/projects/" + projectId + "/tasks/" + taskId;
     }
     @GetMapping("/milestone")
-    public String showTaskMilestone(Model model, @PathVariable Long projectId, @PathVariable Long taskId) {
+    public String addTaskMilestone(Model model, @PathVariable Long projectId, @PathVariable Long taskId) {
         model.addAttribute("projectId",projectId);
         model.addAttribute("taskId",taskId);
-        return  "";
+        //TODO
+        // 프로젝트의 모든 마일스톤 리스트
+        //model.addAttribute("milestoneList",milestoneList);
+        return  "newTaskMilestone";
     }
+    @PostMapping("/milestone/add")
+    public String addMilestone(@PathVariable Long projectId, @PathVariable Long taskId) {
+        //TODO
+        // 테스크 안의 마일스톤 추가 로직
 
-    @PostMapping("/milestone/delete")
-    public String deleteMilestone(@PathVariable Long projectId, @PathVariable Long taskId) {
+        return "redirect:/projects/" + projectId + "/tasks/" + taskId;
+    }
+    @DeleteMapping("/milestone/delete")
+    public String deleteTaskMilestone(@PathVariable Long projectId, @PathVariable Long taskId) {
         //TODO
         // 테스크 안의 마일스톤 삭제 로직
 
